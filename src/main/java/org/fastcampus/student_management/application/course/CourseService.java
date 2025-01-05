6f42b98dcad468f7d713372f5c15bd5c41251332
parkjunki <zisikzz@naver.com>
2025-01-05 23:09:59 +0900

    Initial commit

---
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -0,0 +1,49 @@
+package org.fastcampus.student_management.application.course;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
+import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.DayOfWeek;
+import org.fastcampus.student_management.domain.Student;
+import org.fastcampus.student_management.repo.CourseRepository;
+
+public class CourseService {
+  private final CourseRepository courseRepository;
+  private final StudentService studentService;
+
+  public CourseService(CourseRepository courseRepository, StudentService studentService) {
+    this.courseRepository = courseRepository;
+    this.studentService = studentService;
+  }
+
+  public void registerCourse(CourseInfoDto courseInfoDto) {
+    Student student = studentService.getStudent(courseInfoDto.getStudentName());
+    Course course = new Course(student, courseInfoDto.getCourseName(), courseInfoDto.getFee(), courseInfoDto.getDayOfWeek(), courseInfoDto.getCourseTime());
+    courseRepository.save(course);
+  }
+
+  public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
+    // TODO: 과제 구현 부분
+    List<Course> courseList = courseRepository.getCourseDayOfWeek(dayOfWeek);
+
+    return courseList.stream()
+            .map(course -> new CourseInfoDto(course.getCourseName()
+                    , course.getFee()
+                    , course.getDayOfWeek().toString()
+                    , course.getStudentName()
+                    , course.getCourseTime())).collect(Collectors.toList());
+  }
+
+  //특정 학생의 수강료를 변경 시키면 특정 학생 수업에 전체에 적용이 되어야 함
+  public void changeFee(String studentName, int fee) {
+    // TODO: 과제 구현 부분
+    List<Course> courseList = courseRepository.getCourseListByStudent(studentName);
+
+    for(Course course : courseList) {
+      course.setFee(fee);
+    }
+  }
+}
---
+++ src/main/java/org/fastcampus/student_management/application/student/StudentService.java
@@ -0,0 +1,37 @@
+package org.fastcampus.student_management.application.student;
+
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
+import org.fastcampus.student_management.domain.Student;
+import org.fastcampus.student_management.repo.StudentRepository;
+
+public class StudentService {
+
+  private final StudentRepository studentRepository;
+
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  public void saveStudent(StudentInfoDto studentInfoDto) {
+    Student student = new Student(studentInfoDto.getName(), studentInfoDto.getAge(), studentInfoDto.getAddress());
+    studentRepository.save(student);
+  }
+
+  public Student getStudent(String name) {
+    return studentRepository.findByName(name)
+        .orElseThrow(() -> new IllegalArgumentException("해당하는 학생이 없습니다."));
+  }
+
+  //활동 상태에서 활동 상태로, 비활성 상태에서 비활성 상태로 변경이 되어서는 안 됨
+  public void activateStudent(String name) {
+    // TODO: 과제 구현 부분
+    Student student = getStudent(name);
+    if(!student.isActivate()) student.setActivated(true);
+  }
+
+  public void deactivateStudent(String name) {
+    // TODO: 과제 구현 부분
+    Student student = getStudent(name);
+    if(student.isActivate()) student.setActivated(false);
+  }
+}
